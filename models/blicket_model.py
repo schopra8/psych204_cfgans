ab1c5aadad7131cea3a41712f3fcb321d18a1d0d
schopra8 <schopra8@stanford.edu>
2017-11-13 10:32:01 -0800

    Generate More Data + GAN Training

--- models/blicket_model.py
+++ models/blicket_model.py
@@ -15,6 +15,7 @@ from torch.autograd import Variable
 import json
 import shutil
 from random import seed, shuffle
+import matplotlib.pyplot as plt
 
 # Set random seed
 seed(3)
@@ -67,8 +68,8 @@ class Discriminator(nn.Module):
 
 def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
     torch.save(state, filename)
-    if is_best:
-        shutil.copyfile(filename, 'model_best.pth.tar')
+    # if is_best:
+    #     shutil.copyfile(filename, 'model_best.pth.tar')
 
 
 def train(num_epochs, save_epochs, data):
@@ -87,13 +88,19 @@ def train(num_epochs, save_epochs, data):
     # Optimizers
     criterion = nn.CrossEntropyLoss()
     lr = 1e-4
-    d_optimizer = optim.Adam(D.parameters())
-    g_optimizer = optim.Adam(G.parameters())    
+    d_optimizer = optim.Adam(D.parameters(), lr=lr)
+    g_optimizer = optim.Adam(G.parameters(), lr=lr)
+
+    d_losses = []
+    g_losses = []    
 
     for epoch in range(num_epochs):
         print "Epoch {} of {}".format(epoch + 1, num_epochs)
+        d_errors = []
+        g_errors = []
+
         for i, minibatch in enumerate(data):
-            minibatch = Variable(minibatch, requires_grad=False)
+            minibatch = Variable(minibatch)
             num_samples = minibatch.size()[0]
 
             # Train Discriminator on Real Data
@@ -103,31 +110,37 @@ def train(num_epochs, save_epochs, data):
             d_real_error.backward()
 
             # Train Discriminator on Fake Data
-            gaussian_sample = torch.FloatTensor(num_samples, g_input_size)
-            gaussian_sample.normal_()
-            gaussian_sample = Variable(gaussian_sample, requires_grad=False)
+            sample = torch.FloatTensor(num_samples, g_input_size)
+            sample.normal_()
+            gaussian_sample = Variable(sample)
             d_fake_input = G(gaussian_sample).detach()
             d_fake_preds = D(d_fake_input)
             d_fake_error = criterion(d_fake_preds, Variable(torch.zeros(num_samples)).type(torch.LongTensor))
             d_fake_error.backward()
+            d_errors.append(d_fake_error.data[0])
+            d_errors.append(d_real_error.data[0])
             d_optimizer.step() # Update Discriminator's params now that we've processed real and fake
 
             # Train Generator
-            if epoch >= 10:
+            if epoch >= 5 and i % 2 == 0:
                 G.zero_grad()
-                gaussian_sample = torch.FloatTensor(num_samples, g_input_size)
-                gaussian_sample.normal_()
-                gaussian_sample = Variable(gaussian_sample, requires_grad=False)
-                gen_outputs = G(gaussian_sample)
-                gen_outputs_decision = D(gen_outputs)
-                g_error = criterion(gen_outputs_decision, Variable(torch.ones(num_samples).type(torch.LongTensor)))
+                gaussian_sample = Variable(sample)
+                d_fake_input = G(gaussian_sample)
+                d_fake_preds = D(d_fake_input)
+                g_error = criterion(d_fake_preds, Variable(torch.ones(num_samples).type(torch.LongTensor)))
                 g_error.backward()
                 g_optimizer.step() # Update Generator's parameters
-        
-            if i % 50 == 0:
-                print "Iteration {} of {} ---- Discriminator Loss: {}".format(i, len(data), np.mean([d_fake_error.data[0], d_real_error.data[0]]))
-                if epoch >= 10:
-                    print "Iteration {} of {} ---- Generator Loss: {}".format(i, len(data), g_error.data[0])
+                g_errors.append(g_error.data[0])
+
+        d_epoch_avg_loss = np.mean(d_errors)
+        d_losses.append(d_epoch_avg_loss)
+
+        if epoch >= 5:
+            g_epoch_avg_loss = np.mean(g_errors)
+            g_losses.append(g_epoch_avg_loss)
+            print "Discriminator Loss: {} Generator Loss: {}".format(d_epoch_avg_loss, g_epoch_avg_loss)
+        else:
+            print "Discriminator Loss: {}".format(d_epoch_avg_loss)
 
         if epoch % save_epochs == 0:
             save_checkpoint({
@@ -141,10 +154,10 @@ def train(num_epochs, save_epochs, data):
                 'optimizer': g_optimizer.state_dict()
             }, True, filename='g_checkpoint.pth.tar')
 
-    return D, G, g_input_size
+    return D, G, g_input_size, d_losses, g_losses
 
 def produce_samples(G, g_input_size):
-    gaussian_sample = torch.FloatTensor(10, g_input_size)
+    gaussian_sample = torch.FloatTensor(100, g_input_size)
     gaussian_sample.normal_()
     gaussian_sample = Variable(gaussian_sample, requires_grad=False)
     gen_outputs = G(gaussian_sample)
@@ -152,6 +165,43 @@ def produce_samples(G, g_input_size):
 
 if __name__ == '__main__':
     blicket_data = parse_blicket_data('../gen_data/data/blicket_test_40_p.json')
-    D, G, g_input_size = train(num_epochs=100, save_epochs=10, data=blicket_data)
-    print produce_samples(G, g_input_size)
+    D, G, g_input_size, d_losses, g_losses = train(num_epochs=50, save_epochs=10, data=blicket_data)
+    
+    outputs = produce_samples(G, g_input_size)
+    num_correct = 0
+    num_incorrect = 0
+    num_correct_with_large_b = 0
+    num_correct_with_small_b = 0
+    num_incorrect_with_small_b = 0
+    num_incorrect_with_large_b = 0
+    for o in outputs.split(1):
+        if o.data[0] >= 0.4 and o.data[2] >= 0.4 and o.data[3] >= 0.80:
+            num_correct += 1
+            if o.data[1] <= 0.5:
+                num_correct_with_small_b += 1
+            else:
+                num_correct_with_large_b += 1
+        else:
+            num_incorrect += 1
+            if o.data[1] <= 0.5:
+                num_incorrect_with_small_b += 1
+            else:
+                num_incorrect_with_large_b += 1            
+
+    print '-'*80
+    print "Num Correct: {}".format(num_correct)
+    print "Num Incorrect: {}".format(num_incorrect)
+    print "Num Correct with Small B: {}".format(num_correct_with_small_b)
+    print "Num Correct with Large B: {}".format(num_correct_with_large_b)
+    print "Num Incorrect with Small B: {}".format(num_incorrect_with_small_b)
+    print "Num Incorrect wtih Large B: {}".format(num_incorrect_with_large_b)
+
+    d_plot = plt.plot(range(1, 51), d_losses, 'r--', label="Discriminator")
+    g_plot = plt.plot(range(1, 51), g_losses, 'b++', label="Generator")
+    plt.title('Loss vs. Time')
+    plt.legend(handles=[d_plot, g_plot])
+    plt.show()
+
+
+
 
