0d2a3154ce81bfea2e99dfa3f445cc6a02b0c25e
schopra8 <schopra8@stanford.edu>
2017-12-02 10:49:31 -0800

    GPU + Plotting Fully Operational

--- models/plot_losses.py
+++ models/plot_losses.py
@@ -7,6 +7,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--dfile', help='discriminator losses file', default='./losses/d_losses_100000.txt')
     parser.add_argument('--gfile', help='generator losses file', default='./losses/g_losses_100000.txt')
+    parser.add_argument('--first_discr_epochs', help='# epochs discriminator run first', default=0)
+    parser.add_argument('--data_size', help='data size', default=100000)
     args = parser.parse_args()
 
     with open(args.dfile,"rb") as fp1:
@@ -19,7 +21,7 @@ if __name__ == '__main__':
             fig, ax = plt.subplots()
             num_epochs = len(d_losses)
             ax.plot(range(1, num_epochs + 1), d_losses, 'r', label="Discriminator")
-            ax.plot(range(first_discrim_epochs + 1, num_epochs + 1), g_losses, 'b', label="Generator")
+            ax.plot(range(args.first_discr_epochs + 1, num_epochs + 1), g_losses, 'b', label="Generator")
             plt.title('Loss vs. Time')
             plt.legend()
-            fig.savefig('loss_plot{}'.format(args.data_size))
+            fig.savefig('./losses/loss_plot{}'.format(args.data_size))
