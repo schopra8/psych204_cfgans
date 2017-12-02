10245e7373bb678a30c09edae4d99c970b62c56c
schopra8 <schopra8@stanford.edu>
2017-12-02 13:46:44 -0800

    Summary Stats Version 1

---
+++ models/counterfactual_analysis.py
@@ -0,0 +1,8 @@
+# --------------------------------------------------------------------------------------------------
+# Author: Sahil Chopra
+# Psych 204, Fall 2017
+# Goal: Given a Generator Model, load its weights and sample its worlds. 
+#       When we find a sample that seems to produce a realistic world,
+#       continue to perturb this vector for a 100 iterations according a perturbation format,
+#       note the distribution of worlds that are produced.
+# --------------------------------------------------------------------------------------------------
