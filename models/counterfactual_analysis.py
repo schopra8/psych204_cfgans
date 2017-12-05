72b270cfdf53101b294e92acf9a7397ef089cac1
schopra8 <schopra8@stanford.edu>
2017-12-05 02:40:21 -0800

    First perturbation implementation

--- models/counterfactual_analysis.py
+++ models/counterfactual_analysis.py
@@ -10,6 +10,8 @@
 import argparse
 import os
 import torch
+import random
+import numpy.random.normal as normal
 from blicket_model import Generator, on_gpu, produce_samples
 
 if __name__ == '__main__':
@@ -35,14 +37,48 @@ if __name__ == '__main__':
             print("=> no checkpoint found at '{}'".format(args.model))
 
     samples = produce_samples(G, g_input_size)
-    print samples
+    counterfactualize(G, z, samples)
+
+def check_correct(result, cut_off, stats):
+    if result[0] >= cut_off and result[2] >= cut_off and result[3] >= 0.90:
+        return True, 'D_ON'
+    elif result[0] <= cut_off and result[3] < 0.90:
+        if result[2] <= cut_off:
+            return True, 'D_OFF_A_OFF_C_OFF'
+        else:
+            return True, 'D_OFF_A_OFF_C_ON'
+    elif result[2] <= cut_off and result[3] < 0.90:
+        return True, 'D_OFF_A_ON_C_OFF'
+    else:
+        return False, None
+
+def flip(s):
+    return random.uniform(0, 1) >= s
+
+def prior():
+    return normal()
+
+def counterfactualize(G, Z, samples):
+    for s in samples:
+        valid_sample, state = check_correct(s)
+        if valid_sample:
+            print 'state: {}'.format(state)
+            print 'original: {}'.format(Z[i, :])
+            print perturbation_one(Z[i, :])
+            break
+    pass
 
 
-def perturbation_one():
-    # z_i' = flip(s) ? z_i : prior(z_i)  
-    pass 
+def perturbation_one(z, s=0.5):
+    z_i = z.clone()
+    for j in z.size()[1]
+        if not flip(s):
+            z_i[j] = normal()
+    return z_i
 
 def perturbation_two():
+    z_i = z.clone()
+    # z_i = normal
     # z_i' = N(z_i, sigma)
     pass
 
