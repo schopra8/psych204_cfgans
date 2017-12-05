148ccc04073e4cb7db00c4ae4bfa0a61e382f369
schopra8 <schopra8@stanford.edu>
2017-12-05 01:10:50 -0800

    Import statements

--- models/counterfactual_analysis.py
+++ models/counterfactual_analysis.py
@@ -8,6 +8,8 @@
 # --------------------------------------------------------------------------------------------------
 
 import argparse
+import os
+import torch
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
